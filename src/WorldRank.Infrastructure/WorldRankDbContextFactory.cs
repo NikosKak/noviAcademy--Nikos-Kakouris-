8defd8b61a25f2e5cf23a6a4f05360c33fe057e9
Metalfingers <nickmetalfingers@gmail.com>
2026-07-12 18:15:58 +0300

    Edit ef creation

---
+++ src/WorldRank.Infrastructure/WorldRankDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldRank.Infrastructure
+{
+    public class WorldRankDbContextContextFactory : IDesignTimeDbContextFactory<WorldRankDbContext>
+    {
+        public WorldRankDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WorldRankDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=NoviAcademy;User=nikos;Password=12345;Integrated Security=true;TrustServerCertificate=true");
+
+            return new WorldRankDbContext(optionsBuilder.Options);
+        }
+    }
+}
