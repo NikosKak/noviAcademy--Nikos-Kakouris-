f5cead6ea6d2dee0deea93faa723097d4589db7c
Metalfingers <nickmetalfingers@gmail.com>
2026-07-13 08:20:20 +0300

    Edit Added async funtions

---
+++ src/WorldRank.Infrastructure/Data/DBPlayerRepo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorldRank.Application.Interfaces;
+using WorldRank.Domain.Entities;
+
+namespace WorldRank.Infrastructure.Data
+{
+    internal class DBPlayerRepo : IPlayerRepository
+    {
+        //private field pou krata reference sto context pou tha xrisimopoihthei gia na kanoume ta operations stin vasi dedomenon
+        private readonly WorldRankDbContext _context;
+        //Constructor injection pou dexetai to context kai to apothikeuei sto private field
+        public DBPlayerRepo(WorldRankDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Player?> GetByIdAsync(int id)
+        {
+            return await _context.Players.FindAsync(id);
+        }
+        public async Task AddAsync(Player player)
+        {
+            await _context.Players.AddAsync(player);
+            await _context.SaveChangesAsync();
+        }
+        public void AddPlayer(Player player)
+        {
+            //prosthetei ton player sto context kai apothikeuei tis allages stin vasi dedomenon
+            _context.Players.Add(player);
+            _context.SaveChanges();
+        }
+
+        public void DeletePlayer(int playerId)
+        {
+            var player = _context.Players.Find(playerId);
+            if (player != null) { 
+                _context.Players.Remove(player);
+                _context.SaveChanges();
+            }
+        }
+
+        public Player? FindPlayer(int playerId)
+        {
+            return _context.Players.Find(playerId);
+        }
+
+        public IEnumerable<Player> GetAllPlayers()
+        {
+            return _context.Players.ToList();
+        }
+
+        public IEnumerable<IGrouping<int, Player>> GroupPlayersByScore()
+        {
+            return _context.Players.GroupBy(p => p.Score).ToList();
+        }
+    }
+}
---
+++ src/WorldRank.Infrastructure/Data/DBWalletRepo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorldRank.Application.Interfaces;
+using WorldRank.Domain.Entities;
+using WorldRank.Domain.Enums;
+
+namespace WorldRank.Infrastructure.Data
+{
+    internal class DBWalletRepo : IWalletRepository
+    {
+        private readonly WorldRankDbContext _context;
+
+        public DBWalletRepo(WorldRankDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Wallet wallet)
+        {
+            _context.Wallets.Add(wallet);
+            _context.SaveChanges();
+        }
+
+        public void Block(int playerId, Currency currency)
+        {
+            _context.Wallets.Find(playerId, currency)?.Block();
+            _context.SaveChanges();
+        }
+
+        public void Deposit(int playerId, Currency currency, decimal amount)
+        {
+            _context.Wallets.Find(playerId, currency)?.Deposit(amount);
+            _context.SaveChanges();
+        }
+
+        public Wallet[] GetAll()
+        {
+            return _context.Wallets.ToArray();
+        }
+
+        public List<Wallet> GetAllWalletsByPlayerId(int playerId)
+        {
+            return _context.Wallets.Where(w => w.PlayerId == playerId).ToList();
+        }
+
+        public Wallet GetWallet(int playerId, Currency currency)
+        {
+            var wallet = _context.Wallets
+                .FirstOrDefault(w => w.PlayerId == playerId && w.Currency == currency);
+            if (wallet == null)
+                throw new InvalidOperationException("Wallet not found.");
+            return wallet;
+        }
+
+        public void Unblock(int playerId, Currency currency)
+        {
+            _context.Wallets.Find(playerId, currency)?.Unblock();
+            _context.SaveChanges();
+        }
+
+        public void UpdateBalance(int playerId, Currency currency, decimal newBalance)
+        {
+            var wallet = _context.Wallets.Find(playerId, currency);
+            if (wallet == null)
+                throw new InvalidOperationException("Wallet not found.");
+            wallet.SetBalance(newBalance);
+            _context.SaveChanges();
+        }
+
+        public void Withdraw(int playerId, Currency currency, decimal amount)
+        {
+            var wallet = _context.Wallets.Find(playerId, currency);
+            if (wallet == null)
+                throw new InvalidOperationException("Wallet not found.");
+            wallet.Withdraw(amount);
+            _context.SaveChanges();
+        }
+    }
+}
